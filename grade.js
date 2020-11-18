fd961f0d1518d4b7bcb5e2d47637a97e7c02d28f
Firman Azhar Riyadi <33318547+FirmanAzharR@users.noreply.github.com>
2020-11-18 17:31:37 +0700

    tugas javascript minggu 2

--- grade.js
+++ grade.js
@@ -8,7 +8,7 @@ if(typeof(mtk)=='number' && typeof(bahasaIndonesia)=='number' && typeof(bahasaIn
   
   if(mtk>=0 && mtk<=100 && bahasaIndonesia>=0 && bahasaIndonesia<=100 && bahasaInggris>=0 && bahasaInggris<=100 && ipa>=0 && ipa<=100){
     let jumlah = mtk+bahasaIndonesia+bahasaInggris+ipa;
-    let rata = Math.ceil(jumlah/4);
+    const rata = Math.ceil(jumlah/4);
     if(rata>=90){
       console.log(`Rata-rata = ${rata}
     Grade = A`);
